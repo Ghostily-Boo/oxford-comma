86a64d02c27a5665dae814ab63a3523c9e9703c5
Ghostily-Boo <grace.rl.qin@gmail.com>
2020-10-24 19:59:25 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+    if array.size == 1
+        "#{array.first}"
+    elsif array.size == 2
+        "#{array.first} and #{array.last}"
+    else
+        chopped = array.first array.size - 1
+        chopped.join(", ") << ", and #{array.last}"
+    end
 end
